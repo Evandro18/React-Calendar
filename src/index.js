f2ffeb18fbde8e04c8f20f8c019a8377ae03bd09
Evandro18 <evandro18.goncalves@gmail.com>
2020-04-07 11:15:18 -0400

    fixing bundler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import App from './App'
 
-export default App
+export default { AppCalendar: App }
 // const rootElement = document.getElementById('root')
 // ReactDOM.render(
 //   <React.StrictMode>
