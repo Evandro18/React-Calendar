6ca477d1ce5a089fce7dabf4ec9f6e4539b52c5a
Evandro18 <evandro18.goncalves@gmail.com>
2020-04-07 12:14:47 -0400

    fix export default

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import App from './App'
 
-export const AppCalendar = App
+export default { AppCalendar: App }
 // const rootElement = document.getElementById('root')
 // ReactDOM.render(
 //   <React.StrictMode>
