60e14546b76ab73e5a1bd41c7feb7529a60b3a53
Evandro18 <evandro18.goncalves@gmail.com>
2020-04-07 17:19:36 -0400

    fix(build): fix build and export item

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
 import App from './components/Calendar'
 
-const rootElement = document.getElementById('root')
-ReactDOM.render(
-  <React.StrictMode>
-    <App type='range' />
-  </React.StrictMode>,
-  rootElement
-)
+export const Calendar = App
+
+export default { Calendar }
+// const rootElement = document.getElementById('root')
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App type='range' />
+//   </React.StrictMode>,
+//   rootElement
+// )
