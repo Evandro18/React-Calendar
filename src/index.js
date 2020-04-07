b69fc822d104da3ec5112921c331a05364466c7c
Evandro18 <evandro18.goncalves@gmail.com>
2020-04-07 10:55:17 -0400

    fix(package.json): fix root files

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom'
 
 import App from './App'
 
-const rootElement = document.getElementById('root')
-ReactDOM.render(
-  <React.StrictMode>
-    <App type='range' />
-  </React.StrictMode>,
-  rootElement
-)
+export default App
+// const rootElement = document.getElementById('root')
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App type='range' />
+//   </React.StrictMode>,
+//   rootElement
+// )
