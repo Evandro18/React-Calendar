d66a2848820e8335a2eb885d41943a457dc992e2
tshirotatadashi <tshirotatadashi@gmail.com>
2020-04-08 22:12:13 -0400

    feat(RadioButton): implemented a radio button to select the calendar type

--- src/example/src/App.js
+++ src/example/src/App.js
@@ -1,10 +1,35 @@
-import React from 'react'
-import { RangePicker } from '../../../'
+import React, { useState } from 'react'
+import { Calendar, RangePicker } from '../../../'
+import RadioButton from './components/RadioButton'
+import './style.css'
 
 function App() {
+  const [type, setType] = useState('')
+
+  const typeCalendar = (type) => {
+    switch (type) {
+      case 'calendar':
+        return <Calendar />
+      case 'rangePicker':
+        return <RangePicker label='Select Date' value={[new Date(), new Date('2020-04-13')]} />
+      default:
+        return <RangePicker label='Select Date' value={[new Date(), new Date('2020-04-13')]} />
+    }
+  }
+
+  const CALENDAR_TYPES = [
+    { name: 'Calendar', value: 'calendar' },
+    { name: 'Range Picker', value: 'rangePicker' }
+  ]
+
   return (
     <div className='App'>
-      <RangePicker label='Select Date' value={[new Date(), new Date('2020-04-13')]} />
+      <RadioButton
+        name='typeCalendar'
+        values={CALENDAR_TYPES}
+        onChange={(value) => setType(value)}
+      />
+      <div>{typeCalendar(type)}</div>
     </div>
   )
 }
